ebf9b5c9aae6ca444e96ba84f7806501a1034a32
birisi atuya <tushhamphrey@gamil.com>
2024-02-28 02:24:48 +0300

    configure environment variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '9a5ec8a934ca4b85850d0200258a94ce',
+        key: "9a5ec8a934ca4b85850d0200258a94ce",
     }
 })
