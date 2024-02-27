21a79d0ded9bb5ef90e4858e19b28014271500e1
birisi atuya <tushhamphrey@gamil.com>
2024-02-27 14:09:07 +0300

    Built a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="orange">Game Hub</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" " aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          NAVIGATION BAR
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="gold">
+            SIDEPANEL
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          MAIN AREA
+        </GridItem>
+      </Grid>
     </>
   );
 }
